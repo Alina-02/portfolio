5992f3dae83d203163e53df261260a48232e0091
Alina-02 <alinamontma@gmail.com>
2025-07-05 12:39:37 +0200

    add gradients

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,27 @@ import "./App.css";
 import Flower from "./components/Flower";
 
 const styles = [
-  { font: "ShadigoTrial", circleBackground: "red" },
+  {
+    font: "ShadigoTrial",
+    circleBackground:
+      "linear-gradient(0deg,rgba(166, 3, 3, 1) 54%, rgba(217, 142, 4, 1) 100%)",
+  },
   {
     font: "LemonGarlic",
+    circleBackground:
+      "linear-gradient(0deg,rgba(255, 0, 128, 1) 0%, rgba(179, 255, 0, 1) 72%, rgba(0, 255, 255, 1) 100%)",
   },
-  { font: "Maskdown", circleBackground: "black" },
-  { font: "RochestarDemo" },
+  {
+    font: "Maskdown",
+    circleBackground:
+      "linear-gradient(180deg,rgba(30, 1, 38, 1) 0%, rgba(50, 1, 64, 1) 77%, rgba(63, 2, 89, 1) 100%)",
+  },
+  {
+    font: "RochestarDemo",
+    circleBackground:
+      "linear-gradient(180deg,rgba(242, 216, 216, 1) 0%, rgba(242, 160, 182, 1) 32%, rgba(140, 123, 101, 1) 100%)",
+    sectionsBackground: "rgba(242, 160, 182, 1)",
+  },
   {
     font: "TurquoiseKimono",
     circleBackground:
