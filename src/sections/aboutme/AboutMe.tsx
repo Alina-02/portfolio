f9be9922d74b6f9a50d8fc299e6931627ccf852c
Alina-02 <alinamontma@gmail.com>
2025-07-08 09:39:32 +0200

    reorder code

---
+++ src/sections/aboutme/AboutMe.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { styles } from "../../utils/styles";
+import { TextFade } from "../../components/TextFade";
+import { useTranslation } from "react-i18next";
+
+import "../../styles/aboutme.css";
+
+interface Props {
+  style: number;
+}
+
+const AboutMe = (props: Props) => {
+  const { style } = props;
+  const { t } = useTranslation();
+  return (
+    <div className="about-me-information-wrapper">
+      <div
+        className="about-me-photo"
+        style={{
+          border: `30px solid ${styles[style]?.ringAboutMe}`,
+        }}
+      >
+        <p className="about-me-text">{t("photo progress")}</p>
+      </div>
+      <TextFade
+        staggerChildren={0.7}
+        direction="up"
+        className="about-me-text-container"
+      >
+        <div>
+          <h2
+            style={{
+              fontFamily: `${styles[style]?.font}`,
+              fontSize: "2rem",
+            }}
+          >
+            {t("about me")}
+          </h2>
+          <p className="about-me-text">{t("about me first line")}</p>
+          <p className="about-me-text">{t("about me second line")}</p>
+          <p className="about-me-text">{t("about me third line")}</p>
+        </div>
+      </TextFade>
+    </div>
+  );
+};
+
+export default AboutMe;
---
+++ src/sections/contactme/ContactMe.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { TextFade } from "../../components/TextFade";
+import { styles } from "../../utils/styles";
+
+import "../../styles/contactme.css";
+import { useTranslation } from "react-i18next";
+
+interface Props {
+  style: number;
+}
+
+const ContactMe = (props: Props) => {
+  const { style } = props;
+  const { t } = useTranslation();
+  return (
+    <div
+      className="contact-me-container"
+      style={{
+        backgroundColor: styles[style]?.sectionsBackground,
+        color: styles[style]?.sectionsTextColor,
+      }}
+    >
+      <h2 style={{ fontFamily: `${styles[style]?.font}`, fontSize: "2rem" }}>
+        {t("contactme")}
+      </h2>
+      <TextFade staggerChildren={0.4} direction="up">
+        <div className="contact-me-information-container">
+          <div className="contact-me-element">
+            <img
+              src={"/icons/github.svg"}
+              alt="github icon"
+              style={{
+                width: "75px",
+                height: "75px",
+                filter: `${styles[style]?.iconstFilter}`,
+              }}
+            />
+            <a href="https://github.com/Alina-02" target="_blank">
+              GitHub
+            </a>
+          </div>
+          <div className="contact-me-element">
+            <img
+              src={"/icons/email.svg"}
+              alt="email icon"
+              style={{
+                width: "75px",
+                height: "75px",
+                filter: `${styles[style]?.iconstFilter}`,
+              }}
+            />
+            <a>alinamontma@gmail.com</a>
+          </div>
+          {/*} <a target="_blank" href="https://icons8.com/icon/8808/linkedin">
+            LinkedIn
+          </a>
+          icon by
+          <a target="_blank" href="https://icons8.com">
+            Icons8
+          </a>*/}
+          <div className="contact-me-element">
+            <img
+              src={"/icons/linkedin.svg"}
+              alt="linkedin icon"
+              style={{
+                width: "75px",
+                height: "75px",
+                filter: `${styles[style]?.iconstFilter}`,
+              }}
+            />
+            <a href="https://linkedin.com/in/alina-montoliu-2oo2">LinkedIn</a>
+          </div>
+          <div className="contact-me-element">
+            <img
+              src={"/icons/call.svg"}
+              alt="calls icon"
+              style={{
+                width: "70px",
+                height: "70px",
+                filter: `${styles[style]?.iconstFilter}`,
+              }}
+            />
+            <p style={{ margin: "0" }}>+34 685 63 18 05</p>
+          </div>
+        </div>
+      </TextFade>
+    </div>
+  );
+};
+
+export default ContactMe;
